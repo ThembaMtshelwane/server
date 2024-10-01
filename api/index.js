01844199736cf1a7d918a309d9fe0fc726c3f864
ThembaMtshelwane <thembamm3@gmail.com>
2024-10-01 15:53:09 +0200

    parse from url

--- api/index.js
+++ api/index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const { db } = require("./firebase");
+
+const { parse } = require("url");
 const {
   collection,
   getDocs,
