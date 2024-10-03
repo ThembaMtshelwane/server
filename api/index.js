4af947949e2c73be2ada94f43008d206d28ee193
ThembaMtshelwane <thembamm3@gmail.com>
2024-10-03 11:23:55 +0200

    server update

--- api/index.js
+++ api/index.js
@@ -2,7 +2,6 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const { db } = require("./firebase");
-
 const { parse } = require("url");
 const {
   collection,
@@ -14,11 +13,11 @@ const {
 } = require("firebase/firestore");
 const PORT = 9000;
 
-// const corsOrigin = {
-//   origin: ["http://localhost:3000"],
-// };
 const corsOrigin = {
-  origin: ["https://client-seven-black-21.vercel.app/"],
+  origin: [
+    "http://localhost:3000",
+    "https://client-seven-black-21.vercel.app/",
+  ],
 };
 
 app.use(cors(corsOrigin));
@@ -82,57 +81,6 @@ app.delete("/api/tasks/:id", async (req, res) => {
   }
 });
 
-// app.get("/search", (req, res) => {
-//   /*
-// req.query
-
-// This property is used to access query parameters in the URL.
-// Example: For a request URL like /search?name=John&age=30,
-// req.query.name would be "John" and req.query.age would be "30".
-
-// This URL can be given below or in the broswer as
-// http://localhost:8080/search?name=John&age=30
-// */
-//   const name = req.query.name;
-
-//   /*res.send():
-
-// Sends a response to the client (can be a string, HTML, or other types of content).
-// Example: Sending plain text to the client.
-// */
-//   res.send(`Welcome ${name} to our website`);
-// });
-
-// /*
-// req.params
-
-// This is used to access route parameters in the URL,
-// typically defined in Express routes using the : notation.
-// Example: If the route is defined as /users/:id,
-// and the client requests /users/123, req.params.id would be "123".
-// */
-// app.get("/users/:id", (req, res) => {
-//   console.log(req.params.id); // Outputs: 123
-//   const id = req.params.id;
-
-//   /*
-// res.json()
-//  Sends a JSON response to the client. This is commonly used for APIs to send structured data.
-// */
-//   res.json({
-//     message: `user ${id} data`,
-//     data: [`user ${id} info`, `user ${id} info`, `user ${id} info`],
-//   });
-// });
-
-// /*
-// req.url
-
-// Contains the full URL of the request (relative to the server’s base URL).
-// */
-// app.get("/check", (req, res) => {
-//   console.log(req.url); // Outputs: /check
-// });
 module.exports = (req, res) => {
   const parsedUrl = parse(req.url, true);
   app(req, res, parsedUrl);
--- server.js
+++ server.js
@@ -12,11 +12,11 @@ const {
 } = require("firebase/firestore");
 const PORT = 9000;
 
-// const corsOrigin = {
-//   origin: ["http://localhost:3000"],
-// };
 const corsOrigin = {
-  origin: ["https://client-seven-black-21.vercel.app/"],
+  origin: [
+    "http://localhost:3000",
+    "https://client-seven-black-21.vercel.app/",
+  ],
 };
 
 app.use(cors(corsOrigin));
@@ -79,58 +79,6 @@ app.delete("/api/tasks/:id", async (req, res) => {
     res.status(500).send("Internal Server Error");
   }
 });
-
-// app.get("/search", (req, res) => {
-//   /*
-// req.query
-
-// This property is used to access query parameters in the URL.
-// Example: For a request URL like /search?name=John&age=30,
-// req.query.name would be "John" and req.query.age would be "30".
-
-// This URL can be given below or in the broswer as
-// http://localhost:8080/search?name=John&age=30
-// */
-//   const name = req.query.name;
-
-//   /*res.send():
-
-// Sends a response to the client (can be a string, HTML, or other types of content).
-// Example: Sending plain text to the client.
-// */
-//   res.send(`Welcome ${name} to our website`);
-// });
-
-// /*
-// req.params
-
-// This is used to access route parameters in the URL,
-// typically defined in Express routes using the : notation.
-// Example: If the route is defined as /users/:id,
-// and the client requests /users/123, req.params.id would be "123".
-// */
-// app.get("/users/:id", (req, res) => {
-//   console.log(req.params.id); // Outputs: 123
-//   const id = req.params.id;
-
-//   /*
-// res.json()
-//  Sends a JSON response to the client. This is commonly used for APIs to send structured data.
-// */
-//   res.json({
-//     message: `user ${id} data`,
-//     data: [`user ${id} info`, `user ${id} info`, `user ${id} info`],
-//   });
-// });
-
-// /*
-// req.url
-
-// Contains the full URL of the request (relative to the server’s base URL).
-// */
-// app.get("/check", (req, res) => {
-//   console.log(req.url); // Outputs: /check
-// });
 
 app.listen(PORT, () => {
   console.log("server is running on port", PORT);
